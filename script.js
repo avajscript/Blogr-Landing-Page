168e4c985cfc6202fb56fe79d13756f6069da6f4
Matthew Pierce <lesbianhaircut@outlook.com>
2021-08-16 23:12:11 -0400

    c

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 window.onload = () => {
   // Variables
   const navbarLi = document.querySelectorAll(".li-button");
+  const dropdownButton = document.querySelector("#dropdown-button");
+  const dropDown = document.querySelector(".navbar-popup");
 
   // Functions
 
+  function toggleDropdown() {
+    dropDown.style.display == "none"
+      ? (dropDown.style.display = "block")
+      : (dropDown.style.display = "none");
+  }
+
   function toggleNavItem(event) {
-    let hiddenList = event.target.lastChild.previousElementSibling;
-    let arrowIcon = event.target.firstChild.nextElementSibling;
+    let hiddenList = event.lastChild.previousElementSibling;
+    let arrowIcon = event.firstChild.nextElementSibling;
 
     hiddenList.style.display == "none"
       ? (hiddenList.style.display = "block")
@@ -15,6 +23,22 @@ window.onload = () => {
 
   // Events
   navbarLi.forEach((listItem) => {
-    listItem.addEventListener("click", toggleNavItem);
+    listItem.addEventListener("click", () => {
+      toggleNavItem(listItem);
+    });
+  });
+
+  dropdownButton.addEventListener("click", toggleDropdown);
+
+  window.addEventListener("click", () => {
+    if (
+      !event.target.matches("li") &&
+      !event.target.matches("p") &&
+      !event.target.matches(".sign-up") &&
+      !event.target.matches(".navbar-popup") &&
+      !event.target.matches("#dropdown-button")
+    ) {
+      dropDown.style.display = "none";
+    }
   });
 };
