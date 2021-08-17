75c98941431f16b973150de8aec4b8ad76432e66
Matthew Pierce <lesbianhaircut@outlook.com>
2021-08-17 18:09:52 -0400

    c

--- script.js
+++ script.js
@@ -3,8 +3,29 @@ window.onload = () => {
   const navbarLi = document.querySelectorAll(".li-button");
   const dropdownButton = document.querySelector("#dropdown-button");
   const dropDown = document.querySelector(".navbar-popup");
+  const desktopNavbarLi = document.querySelectorAll(".desktop-nav-item");
 
   // Functions
+
+  function clearDesktopNav() {
+    desktopNavbarLi.forEach((liItem) => {
+      let dropdown = liItem.lastElementChild;
+      if (!dropdown.classList.contains("active")) {
+        dropdown.style.display = "none";
+      }
+      dropdown.classList.remove("active");
+    });
+  }
+
+  function displayDesktopNav(element) {
+    let dropDown = element.lastElementChild;
+    dropDown.classList.add("active");
+
+    clearDesktopNav();
+    dropDown.style.display == "block"
+      ? (dropDown.style.display = "none")
+      : (dropDown.style.display = "block");
+  }
 
   function toggleDropdown() {
     if (dropDown.style.display == "none") {
@@ -20,15 +41,25 @@ window.onload = () => {
     let hiddenList = event.lastChild.previousElementSibling;
     let arrowIcon = event.firstChild.nextElementSibling;
 
-    hiddenList.style.display == "none"
-      ? (hiddenList.style.display = "block")
-      : (hiddenList.style.display = "none");
+    if (hiddenList.style.display == "none") {
+      hiddenList.style.display = "block";
+      arrowIcon.style.transform = "rotate(180deg)";
+    } else {
+      hiddenList.style.display = "none";
+      arrowIcon.style.transform = "rotate(0deg)";
+    }
   }
 
   // Events
   navbarLi.forEach((listItem) => {
     listItem.addEventListener("click", () => {
       toggleNavItem(listItem);
+    });
+  });
+
+  desktopNavbarLi.forEach((listItem) => {
+    listItem.addEventListener("click", () => {
+      displayDesktopNav(listItem);
     });
   });
 
@@ -43,6 +74,7 @@ window.onload = () => {
       !event.target.matches("#dropdown-button")
     ) {
       dropDown.style.display = "none";
+      dropdownButton.src = "./images/icon-hamburger.svg";
     }
   });
 };
